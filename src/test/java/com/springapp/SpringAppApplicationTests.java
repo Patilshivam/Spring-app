e1cdd4d066ae32c060205dd48466d6a604f79966
Patilshivam <shivampatil070299@gmail.com>
2020-07-28 22:19:10 +0530

    Spring App uploding to git!

---
+++ src/test/java/com/springapp/SpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springapp;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
